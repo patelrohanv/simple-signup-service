b9c0446e33b83327394b920c18067b4241644875
Rohan Patel <patel@Rohans-MacBook-Air-2.local>
2023-07-09 16:51:52 -0500

    nuxt-vue

---
+++ nuxt-vue/pages/register.vue
@@ -0,0 +1,43 @@
+<template>
+  <div>
+    <h1>Register</h1>
+    <form @submit.prevent="register">
+      <input v-model="firstName" placeholder="First name" required>
+      <input v-model="lastName" placeholder="Last name" required>
+      <input v-model="email" type="email" placeholder="Email" required>
+      <input v-model="username" placeholder="Username" required>
+      <input v-model="password" type="password" placeholder="Password" required>
+      <button type="submit">Register</button>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      firstName: '',
+      lastName: '',
+      email: '',
+      username: '',
+      password: ''
+    }
+  },
+  methods: {
+    async register() {
+      try {
+        const response = await this.$axios.$post('/api/register', {
+          firstName: this.firstName,
+          lastName: this.lastName,
+          email: this.email,
+          username: this.username,
+          password: this.password
+        })
+        this.$router.push('/users')
+      } catch (error) {
+        console.error(error)
+      }
+    }
+  }
+}
+</script>
---
+++ nuxt-vue/pages/users.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+      <h1>Users</h1>
+      <ul>
+        <li v-for="user in users" :key="user.id">
+          {{ user.firstName }} {{ user.lastName }}
+        </li>
+      </ul>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    data() {
+      return {
+        users: []
+      }
+    },
+    async created() {
+      try {
+        this.users = await this.$axios.$get('/api/users')
+      } catch (error) {
+        console.error(error)
+      }
+    }
+  }
+  </script>
+  
